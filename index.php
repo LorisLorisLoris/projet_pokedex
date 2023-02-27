45cc4de9ba0449c49cd744b89c29caa933bb7775
lorislorisloris <loris.assemat@gmail.com>
2023-02-27 09:47:48 +0100

    update

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
+
+// 1. Afficher tous les pokemons
+// 2. Creer la page showPokemon qui affiche un pokemon via son index dans le tableau
+// 3. Creer la page addPokemon
+// 4. Permettre la suppression d'un pokemon via un lien en GET par l'index
+// 4. Creer la page modifier pokemon et rajouter un button modifier
+
+//recuperer les données pokemons en json
 $json_data = file_get_contents("pokemons.json");
+//decoder le json en tableau
 $pokemons = json_decode($json_data, true);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,7 +21,8 @@ $pokemons = json_decode($json_data, true);
 </head>
 <body>
     <h1>Pokédex</h1>
-    <div class="cards">
+    <section class="cards">
+        <!-- Afficher -->
         <?php foreach ($pokemons as $i => $pokemon): ?>
             <div class="card">
                 <h2><?= $pokemon["name"] ?></h2>
@@ -21,6 +32,6 @@ $pokemons = json_decode($json_data, true);
                 <a href="deletePokemon.php?id=<?= $i ?>" class="delete-link" onclick="return confirm('Êtes-vous sûr de vouloir supprimer ce Pokémon ?')">Supprimer</a>
             </div>
         <?php endforeach; ?>
-    </div>
+    </section>
 </body>
 </html>
